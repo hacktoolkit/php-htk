0714a8bae9ab1f38015f31042f8cc6429c583dbf
Jonathan Tsai <hello@jontsai.com>
2021-10-11 10:40:24 +0000

    v0.1.3 Sets a guard $wasInited

--- src/Htk.php
+++ src/Htk.php
@@ -6,15 +6,20 @@ use Htk\Config;
 use Htk\Debugger;
 
 class Htk {
+    private static $wasInited = false;
     private static $config = null;
     private static $debugger = null;
 
     public static function init(array $configArray) {
         self::$config = new Config($configArray);
         self::$debugger = new Debugger(self::$config);
+
+        self::$wasInited = true;
     }
 
     public static function slack_debug($text, $channel = null) {
-        self::$debugger->slack_debug($text, $channel);
+        if (self::$wasInited) {
+            self::$debugger->slack_debug($text, $channel);
+        }
     }
 }
